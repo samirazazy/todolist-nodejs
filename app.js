32c183d8e98a21feb9fecd020a297cd1f6e5d84e
samirazazy <samiralazazy@gmail.com>
2020-07-07 18:00:06 +0200

    Passing Data from Your Webpage to Your Server

--- app.js
+++ app.js
@@ -3,10 +3,33 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var items = [];
+
 app.get("/", function (req, res) {
   today = new Date();
 
-  today.getDay() === 5 ? res.send("weckend") : res.send("work");
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  };
+
+  var day = today.toLocaleDateString("en-US", options);
+
+  res.render("list", {
+    kondOfDay: day,
+    newListItems: items,
+  });
+});
+
+app.post("/", function (req, res) {
+  var item = req.body.newItem;
+  items.push(item);
+  res.redirect("/");
 });
 
 app.listen(3000, function () {
