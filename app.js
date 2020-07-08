c303933b8e0c6e82e3868e472562695c8cae2537
samir azazy <samiralazazy@gmail.com>
2020-07-08 13:26:49 +0200

    style list page

--- app.js
+++ app.js
@@ -6,19 +6,20 @@ const app = express();
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
-var items = [];
+let items = [];
 
 app.get("/", function (req, res) {
   today = new Date();
 
-  var options = {
+  let options = {
     weekday: "long",
     day: "numeric",
     month: "long",
   };
 
-  var day = today.toLocaleDateString("en-US", options);
+  let day = today.toLocaleDateString("en-US", options);
 
   res.render("list", {
     kondOfDay: day,
@@ -27,7 +28,7 @@ app.get("/", function (req, res) {
 });
 
 app.post("/", function (req, res) {
-  var item = req.body.newItem;
+  let item = req.body.newItem;
   items.push(item);
   res.redirect("/");
 });
