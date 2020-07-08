fedd5aadf9d38e4dcdca03e3ceab844e6c1078b7
samirazazy <samiralazazy@gmail.com>
2020-07-08 20:39:18 +0200

    Refactor date function

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 
@@ -12,15 +13,7 @@ let items = ["Pay Food", "Cook Food", "Eat Foot"];
 let workItems = [];
 
 app.get("/", function (req, res) {
-  today = new Date();
-
-  let options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long",
-  };
-
-  let day = today.toLocaleDateString("en-US", options);
+  const day = date.getDate();
 
   res.render("list", {
     listTitle: day,
